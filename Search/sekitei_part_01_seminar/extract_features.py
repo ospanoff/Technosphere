effa806e1de5c3bb1d7cfc6085c4803fff3ff343
Ayat Ospanov <ayat.ospanov@gmail.com>
2016-02-29 16:12:18 +0300

    Added InfoSearch project

---
+++ Search/sekitei_part_01_seminar/extract_features.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[75]:
+
+import sys
+import re
+import random
+from operator import itemgetter
+
+
+def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
+    print >> sys.stderr, "Not implemented"
+
+
+
+
+
